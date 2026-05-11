8ada8a643afc54715b6701fedbca3aaf7fe19358
athiphat67 <athiphat.sun@dome.tu.ac.th>
2026-05-11 12:00:12 +0700

    feat : update change sqlite3 to postgreSQL

--- myproject/baanhao_project/baanhao_project/settings.py
+++ myproject/baanhao_project/baanhao_project/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'baanhao_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'baanhao_db'),
+        'USER': os.environ.get('DB_USER', 'baanhao_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
